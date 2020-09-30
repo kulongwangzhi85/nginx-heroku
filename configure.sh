05c7b8d45a40435bae8978ee2a07bb5dd140c7ab
guocl <guochanglong85@gmail.com>
2020-09-30 15:25:10 +0800

    fix nginx config error

--- configure.sh
+++ configure.sh
@@ -2,14 +2,14 @@
 
 cat << EOF > /etc/nginx/conf.d/aaa.conf
 server {
-    listen $PORT ssl;
+    listen $PORT ;
     server_name "$SERVERNAME" ;
     #ssl_certificate     /etc/nginx/conf.d/certs/xxxx.pem;
     #ssl_certificate_key /etc/nginx/conf.d/certs/xxxx.key;
-    ssl_session_timeout 5m;
-    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
-    ssl_prefer_server_ciphers on;
+    #ssl_session_timeout 5m;
+    #ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    #ssl_ciphers ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
+    #ssl_prefer_server_ciphers on;
     location / {
         root  /nginx/html/;
         index  index.html;
